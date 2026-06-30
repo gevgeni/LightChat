4ea5d61dc5aeb6b81aec3ef1f0602f01037c7bc5
gevgeni <evge1599@gmail.com>
2026-06-30 15:00:33 +0500

    DTO перенесены в отдельный файл. Добавлен LoginRequest DTO.

---
+++ Web/Models/DTOs.cs
@@ -0,0 +1,7 @@
+﻿namespace LightChat.Web.Models
+{
+    public record CreateUserDto(string Username, string Email);
+    public record CreateChatDto(string Name);
+    public record AddMemberDto(Guid ChatId, Guid UserId);
+    public record LoginRequest(string Username);
+}
