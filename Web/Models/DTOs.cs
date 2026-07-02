862a544585a5295bcd4009a380123416d7916b85
gevgeni <evge1599@gmail.com>
2026-07-02 13:59:53 +0500

    В AddMemberDto удалено свойство ChatId

--- Web/Models/DTOs.cs
+++ Web/Models/DTOs.cs
@@ -2,6 +2,6 @@
 {
     public record CreateUserDto(string Username, string Email);
     public record CreateChatDto(string Name);
-    public record AddMemberDto(Guid ChatId, Guid UserId);
+    public record AddMemberDto(Guid UserId);
     public record LoginRequest(string Username);
 }
