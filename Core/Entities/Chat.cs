9012100c8fb44dc31655ec57d96217c4ca1cf6de
gevgeni <evge1599@gmail.com>
2026-07-03 13:35:30 +0500

    Добавлено свойство IsDirect

--- Core/Entities/Chat.cs
+++ Core/Entities/Chat.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = null!;
+        public bool IsDirect { get; set; } = false;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public ICollection<Message> Messages { get; set; } = [];
