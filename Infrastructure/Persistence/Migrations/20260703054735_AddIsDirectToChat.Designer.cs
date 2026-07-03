670368b8ea9f1248a23be680a6cf7d20155e1760
gevgeni <evge1599@gmail.com>
2026-07-03 13:45:42 +0500

    Добавлены миграции AddIsDirect to Chat и FixRemainingModelChanges

--- Infrastructure/Persistence/Migrations/20260703054735_AddIsDirectToChat.Designer.cs
+++ Infrastructure/Persistence/Migrations/20260703054735_AddIsDirectToChat.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LightChat.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260703054735_AddIsDirectToChat")]
+    partial class AddIsDirectToChat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -30,9 +33,6 @@ namespace Infrastructure.Migrations
 
                     b.Property<DateTime>("CreatedAt")
                         .HasColumnType("timestamp with time zone");
-
-                    b.Property<bool>("IsDirect")
-                        .HasColumnType("boolean");
 
                     b.Property<string>("Name")
                         .IsRequired()
---
+++ Infrastructure/Persistence/Migrations/20260703071228_FixRemainingModelChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRemainingModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDirect",
+                table: "Chats",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDirect",
+                table: "Chats");
+        }
+    }
+}
