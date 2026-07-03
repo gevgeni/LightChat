ccb8cd8dded7556f2f7384f130cf33475b1875e6
gevgeni <evge1599@gmail.com>
2026-07-03 09:28:20 +0500

    Сущностям запросов CreateUserDto и LoginRequest добавлено свойство string Password

--- Web/Models/DTOs.cs
+++ Web/Models/DTOs.cs
@@ -1,7 +1,7 @@
 ﻿namespace LightChat.Web.Models
 {
-    public record CreateUserDto(string Username, string Email);
+    public record CreateUserDto(string Username, string Email, string Password);
     public record CreateChatDto(string Name);
     public record AddMemberDto(Guid UserId);
-    public record LoginRequest(string Username);
+    public record LoginRequest(string Username, string Password);
 }
