6fde5a499261d39ee4d295fb07d34595ab4af31f
gevgeni <evge1599@gmail.com>
2026-07-03 09:27:05 +0500

    Добавлено свойство PasswordHash

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -5,6 +5,8 @@
         public Guid Id { get; set; }
         public string Username { get; set; } = null!;
         public string Email { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         public ICollection<ChatMember> ChatMembers { get; set; } = [];
