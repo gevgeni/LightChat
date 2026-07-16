1592a39a2eae30e12cedbf4bf20488d005a92497
KonKhiDot <drmigreniy5@gmail.com>
2026-07-16 21:55:09 +0500

    Добавлены классы-маркеры для слоев Core и Infrastructure для того, чтобы в program.cs настроить DI

---
+++ Core/AssemblyMarker.cs
@@ -0,0 +1,10 @@
+﻿namespace LightChat.Core
+{
+    /// <summary>
+    /// Пустой класс-маркер, используемый исключительно для того, 
+    /// чтобы в механизмах Dependency Injection (DI) определять сборку (Assembly) Core.
+    /// </summary>
+    public abstract class AssemblyMarker
+    {
+    }
+}
---
+++ Infrastructure/AssemblyMarker.cs
@@ -0,0 +1,10 @@
+﻿namespace LightChat.Infrastructure
+{
+    /// <summary>
+    /// Пустой класс-маркер, используемый исключительно для того, 
+    /// чтобы в механизмах Dependency Injection (DI) определять сборку (Assembly) Core.
+    /// </summary>
+    public abstract class AssemblyMarker
+    {
+    }
+}
