8f4f409df7f1a3c4893b83ca066892913e78d390
KonKhiDot <drmigreniy5@gmail.com>
2026-07-16 21:46:58 +0500

    Dto переименованы в Requests, тк выполняют функцию запросов данных в endpoint-ах

--- Web/Models/DTOs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LightChat.Web.Models
-{
-    public record CreateUserDto(string Username, string Email, string Password);
-    public record CreateChatDto(string Name);
-    public record AddMemberDto(Guid UserId);
-    public record LoginRequest(string Username, string Password);
-    public record CreateDirectChatRequest(Guid TargetUserId);
-}
---
+++ Web/Requests/Requests.cs
@@ -0,0 +1,8 @@
+﻿namespace LightChat.Web.Requests
+{
+    public record CreateUserRequest(string Username, string Email, string Password);
+    public record CreateChatRequest(string Name);
+    public record AddMemberRequest(Guid UserId);
+    public record LoginRequest(string Username, string Password);
+    public record CreateDirectChatRequest(Guid TargetUserId);
+}
