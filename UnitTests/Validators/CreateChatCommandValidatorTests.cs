d9f0efabd3b9d8744f7988e742b404892b38e7c7
gevgeni <evge1599@gmail.com>
2026-07-23 10:41:33 +0500

    Добавлены unit-тесты для валидаторов: CreateChatValidator, UserJwtAuthorizeValidator и UserRegisterCommandValidatorTests

---
+++ UnitTests/Validators/CreateChatCommandValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentValidation.TestHelper;
+using LightChat.Core.Features.Chats;
+
+namespace LightChat.Core.Tests.Validators
+{
+    public class CreateChatCommandValidatorTests
+    {
+        private readonly CreateChatValidator _validator = new();
+
+        #region Have_Error
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("ab")]
+        public void Should_Have_Error_When_Name_Is_Invalid(string invalidName)
+        {
+            var command = new CreateChatCommand(invalidName, Guid.NewGuid());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(c => c.Name);
+        }
+
+        [Fact]
+        public void Should_Have_Error_When_Creator_Guid_Is_Invalid()
+        {
+            var command = new CreateChatCommand("validName", Guid.Empty);
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(c => c.CreatorUserId);
+        }
+        #endregion
+
+        #region Not_Have_Error
+        [Theory]
+        [InlineData("validName")]
+        [InlineData("goketsu")]
+        public void Should_Not_Have_Error_When_Name_Is_Valid(string validName)
+        {
+            var command = new CreateChatCommand(validName, Guid.NewGuid());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveValidationErrorFor(c => c.Name);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Creator_Guid_Is_Valid()
+        {
+            var command = new CreateChatCommand("validName", Guid.NewGuid());
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveValidationErrorFor(c => c.CreatorUserId);
+        }
+        #endregion
+    }
+}
---
+++ UnitTests/Validators/UserJwtAuthorizeCommandValidatorTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation.TestHelper;
+using LightChat.Core.Features.Users;
+
+namespace LightChat.Core.Tests.Validators
+{
+    public class UserJwtAuthorizeCommandValidatorTests
+    {
+        private readonly UserJwtAuthorizeValidator _validator = new();
+
+        #region Have_Error
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("ab")]
+        public void Should_Have_Error_When_Username_Is_Invalid(string invalidUsername)
+        {
+            var query = new UserJwtAuthorizeQuery(invalidUsername, "Password123!");
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(c => c.Username);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("12345")]
+        public void Should_Have_Error_When_Password_Is_Invalid(string invalidPassword)
+        {
+            var query = new UserJwtAuthorizeQuery("ValidUser", invalidPassword);
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldHaveValidationErrorFor(c => c.Password);
+        }
+        #endregion
+
+        #region Not_Have_Error
+        [Theory]
+        [InlineData("validName")]
+        [InlineData("goketsu")]
+        public void Should_Not_Have_Error_When_Username_Is_Valid(string validUsername)
+        {
+            var query = new UserJwtAuthorizeQuery(validUsername, "Password123!");
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveValidationErrorFor(c => c.Username);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_When_Password_Is_Valid()
+        {
+            var query = new UserJwtAuthorizeQuery("ValidUser", "Password123!");
+
+            var result = _validator.TestValidate(query);
+
+            result.ShouldNotHaveValidationErrorFor(c => c.Password);
+        }
+        #endregion
+    }
+}
